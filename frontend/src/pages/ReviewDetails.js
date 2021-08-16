c7581e0dd63c155fbc3895fe8f36e2ea5bbae790
developer-junaid <qjunaid623@gmail.com>
2021-08-16 10:57:38 +0500

    completed review details section

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,9 +1,37 @@
 import React from "react";
 
+// Hooks
+import { useParams } from "react-router-dom";
+import useFetch from "./../hooks/useFetch";
+
+// Details
 const ReviewDetails = () => {
+  // Use Params
+  const { id } = useParams();
+
+  // Get Single Review
+  const url = `http://localhost:1337/reviews/${id}`;
+  const { data, error, loading } = useFetch(url);
+
+  // Loading
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+
+  // Error
+  if (error) {
+    return <p>Error !</p>;
+  }
+
+  // Return
   return (
-    <div>
-      <h1>Review Details</h1>
+    <div className="review-card">
+      <div className="rating">{data.rating}</div>
+      <h2>{data.title}</h2>
+
+      <small>console list</small>
+
+      <p>{data.body}</p>
     </div>
   );
 };
